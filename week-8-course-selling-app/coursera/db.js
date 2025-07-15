57519ea894c3291f0c733ab9a55ed963e7616735
Mangloo69 <yadav69.swayam@gmail.com>
2025-07-16 02:13:55 +0545

    DB schema setup

---
+++ week-8-course-selling-app/coursera/db.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const userSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+
+const adminSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+
+const courseSchema = new Schema({
+  title: String,
+  description: String,
+  price: Number,
+  imageUrl: String,
+  creatorId: ObjectId,
+});
+
+const purchaseSchema = new Schema({
+  courseId: ObjectId,
+  userId: ObjectId,
+});
+
+const UserModel = mongoose.model("users", userSchema);
+const AdminModel = mongoose.model("admins", adminSchema);
+const CourseModel = mongoose.model("courses", courseSchema);
+const PurchaseModel = mongoose.model("purchases", purchaseSchema);
+
+module.exports = {
+  UserModel,
+  AdminModel,
+  CourseModel,
+  PurchaseModel,
+};
