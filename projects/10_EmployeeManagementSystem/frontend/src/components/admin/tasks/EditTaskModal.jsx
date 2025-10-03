b58a2ba645c3142bbe4cad836157d3aef9e81a20
Mangloo69 <yadav69.swayam@gmail.com>
2025-10-04 01:29:04 +0545

    build error fix

--- projects/10_EmployeeManagementSystem/frontend/src/components/admin/tasks/EditTaskModal.jsx
+++ projects/10_EmployeeManagementSystem/frontend/src/components/admin/tasks/EditTaskModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Button from "../../ui/Button";
-import Input from "../../ui/input";
+import Input from "../../ui/Input";
 import {
   Card,
   CardContent,
--- projects/10_EmployeeManagementSystem/frontend/src/components/auth/LoginSignup.jsx
+++ projects/10_EmployeeManagementSystem/frontend/src/components/auth/LoginSignup.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../ui/Button";
-import Input from "../ui/input";
+import Input from "../ui/Input";
 import {
   Card,
   CardContent,
